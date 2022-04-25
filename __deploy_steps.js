d7db25c28118463cff4aa88ae505e94c9e432136
ArafatMunna <arafatmunna000@gmail.com>
2022-04-26 02:03:11 +0600

    steps added

---
+++ __deploy_steps.js
@@ -0,0 +1,30 @@
+/* 
+     ----------------------------
+    ONE TIME for your computer
+    ----------------------------
+    1. Create heroku account
+    2. Verify Email
+    3. install heroku cli
+    4. heroku login
+    ----------------------------
+    For each project one time
+    ----------------------------
+    1. heroku create
+    2. make sure you: git add . git commit . git push
+    3. git push heroku main
+    4. Go to heroku Dashboard > Current Project > Settings > Revel Config Vars
+    5. Copy paste config vars from .env file
+    6. Make sure you have whitelisted all ip address to access mongodb
+    ----------------------------
+    UPDATE SERVER with changes
+    ----------------------------
+    1. Make Changes
+    2. make sure you: git add . git commit . git push
+    3. git push heroku main
+    ---------------------------------------------
+    Connect Server with Client and deploy client
+    ---------------------------------------------
+    1. replace localhost bay heroku link
+    2. npm run build
+    3. firebase deploy
+*/
